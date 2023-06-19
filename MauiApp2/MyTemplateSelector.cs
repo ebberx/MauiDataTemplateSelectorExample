9177d74960665042415fc37e10fff2c938a7d08a
Ebberx <esben_dahl@hotmail.com>
2023-06-19 22:17:50 +0200

    Initial example commit

---
+++ MauiApp2/MyTemplateSelector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp2
+{
+    public class MyTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate FruitTemplate { get; set; }
+        public DataTemplate AnimalTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container) {
+            return item is Fruit ? FruitTemplate : AnimalTemplate;
+        }
+    }
+}
---
+++ MauiApp2/ViewModels/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿namespace MauiApp2.ViewModels;
+
+public class Fruit {
+    public string Name { get; set; } = "Banana";
+    public int Quantity { get; set; } = 2;
+}
+
+public class Animal
+{
+    public string Name { get; set; } = "Hyena";
+    public string Sound { get; set; } = "huehuehuehue";
+}
+
+public partial class MainViewModel : BaseViewModel {
+    [ObservableProperty]
+    public List<Object> items = new List<Object>() {
+
+        new Fruit { },
+        new Fruit { Name = "Apple", Quantity = 2 },
+        new Fruit { Name = "Pear", Quantity = 5 },
+        new Fruit { Name = "Orange", Quantity = 3 },
+        new Animal { Name = "Orangutan", Sound = "Me find banana, me eat banana" },
+        new Animal { Name = "EASV Student", Sound = "Oh boy! I love UX design!" },
+        new Animal { }
+    };
+
+}
